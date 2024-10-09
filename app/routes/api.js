bedd2ec0661eccfa1659a86740cd01c5dae5553e
Suzuki <joaosuzuki98@outlook.com>
2024-10-09 14:30:52 -0300

    feat(routes): Add tasks routes

--- app/routes/api.js
+++ app/routes/api.js
@@ -2,9 +2,13 @@ const express = require('express')
 const router = express.Router()
 
 // Import controllers here
+const taskController = require('../controllers/taskController') 
 
 // Routes here
-
-
+router.post('/tasks', taskController.createTask)
+router.get('/tasks', taskController.showManyTasks)
+router.get('/tasks/:name', taskController.showOneTask)
+router.put('/tasks/:name', taskController.updateTask)
+router.delete('/tasks/:name', taskController.deleteTask)
 
 module.exports = router
